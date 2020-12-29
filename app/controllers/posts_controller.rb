10ded448f4069007ad06a18e5d2bce7b1bb36eaa
mi4seven <mi4seven@gmail.com>
2020-12-30 01:15:20 +0200

    Add Method Create Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,38 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+
     def index
         @posts = Post.all.order("created_at DESC")
+        @post = Post.new
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        #@post = Post.new(post_params)
+        @post = Post.new(params[:post])        
+        @post.save
+
+        redirect_to root_path notice: 'Post was successfully created.'
+            
+        # if @post.save
+        #     redirect_to post_path(@post)
+        # redirect_to root_path notice: 'Post was successfully created.'    
+        # else
+        #     render :new
+        # end
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+        @post = Post.find(params[:id])
+    end    
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end      
 end
